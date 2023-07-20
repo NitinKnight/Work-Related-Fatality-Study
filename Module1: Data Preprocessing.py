b57b8f9fc74bf23283e7f6a6ee5e6c89448c506d
Nitin Kayyath <55591127+NitinKnight@users.noreply.github.com>
2023-07-20 19:02:38 +0100

    Create Module1: Data Preprocessing.py
    module1: Data Preprocessing file with added comments:

---
+++ Module1: Data Preprocessing.py
@@ -0,0 +1,39 @@
+# Importing necessary libraries for data processing
+import pandas as pd
+import numpy as np
+
+# Task 1: Function to read the provided dataset
+def read_data_from_csv():
+    # Read data from 'fatality.csv' into a dataframe
+    df = pd.read_csv('fatality.csv') 
+    return df
+
+# Task 2: Function to clean the dataset by removing unnecessary columns
+def data_cleaning():
+    # Obtain the dataset by reading from CSV
+    df = read_data_from_csv()
+    
+    # Identify columns starting with 'Unnamed' and store them in a list
+    Unnamed_columns = [col for col in df.columns if col.startswith('Unnamed')]
+    
+    # Drop these 'Unnamed' columns from the dataframe
+    df = df.drop(columns=Unnamed_columns)
+    
+    return df
+
+# Task 3: Function to export the cleaned dataset to a new CSV file
+def export_the_dataset():
+    # Fetch the cleaned dataset
+    df = data_cleaning()
+    
+    # Save the cleaned dataset to 'fatalities_cleaned.csv' with indexing turned off
+    df.to_csv('fatalities_cleaned.csv', index=False)
+    
+    return df
+
+# Task 4: This section pertains to loading the cleaned dataset into the provided database.
+# Detailed steps need to be followed as per the Task 4 instructions.
+
+# Ensuring that the MySQL table has been created using "fatalities_cleaned"
+# This final cleaned dataset will be uploaded onto the provided database for subsequent analysis in MySQL.
+# To execute this task, navigate to the terminal and initiate the project run.
