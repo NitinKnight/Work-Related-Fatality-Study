a390c36b2985dc205ef13ef2d4ebb27c1d4a030f
Nitin Kayyath <55591127+NitinKnight@users.noreply.github.com>
2023-07-20 18:57:56 +0100

    Created db.py
    added comments for each line in the db.py file

---
+++ db.py
@@ -0,0 +1,13 @@
+# Database configurations for the application
+
+# The host IP or domain name where the database server is running
+HOST = ''
+
+# The username used to authenticate and access the database
+USERNAME = ''
+
+# The password associated with the given username for database access
+PASSWORD = ''
+
+# The specific database to connect to on the server
+DATABASE = ''
